0ddc4ab08c2c0aa76ebb190c51f77fb0ad0510a5
Kiet Luong <ltkiet194@gmail.com>
2023-06-19 16:05:49 +0700

    Store Build

--- GameStore/Controllers/StoreController.cs
+++ GameStore/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,38 @@ namespace GameStore.Controllers
 {
     public class StoreController : Controller
     {
+        DataGameStoreDataContext data = new DataGameStoreDataContext();
         // GET: Store
         public ActionResult Store()
         {
             ViewBag.TrangChu = 1;
             ViewBag.Store = 0;
-            return View();
+            int iSize = 6;
+   
+            var list10Game = data.Games.ToList().Take(10);;
+
+            return View(list10Game);
         }
+        public ActionResult PartialFeaturedGame()
+        {
+            ViewBag.TrangChu = 1;
+            ViewBag.Store = 0;
+            int iSize = 6;
+
+            var list4Game = data.Games.ToList().Take(4).Where(n => n.id <10); ;
+
+            return PartialView(list4Game);
+        }
+        public ActionResult PartialMostPopular()
+        {
+            ViewBag.TrangChu = 1;
+            ViewBag.Store = 0;
+            int iSize = 6;
+
+            var list6Game = data.Games.ToList().Take(9).Where(n => n.id < 10); ;
+
+            return PartialView(list6Game);
+        }
+
     }
 }
