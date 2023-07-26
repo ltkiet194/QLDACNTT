0949116c718e41aa54bcd1df2abe91cc272fed1a
QuyDevit <ngocquydev02@gmail.com>
2023-07-26 17:27:50 +0700

    Final Project GameStore

---
+++ GameStore/Models/DTO/Statistical.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.Models.DTO
+{
+    public class Statistical
+    {
+        public string NameKH { get; set; }
+        public string Namegame { get; set; }
+        public int Quantity { get; set; }
+        public float PriceGame { get; set; }
+        public float Total { get; set; }
+
+    }
+}
